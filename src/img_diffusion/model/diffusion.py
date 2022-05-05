e9823ba03fe1f2fc1c7b624de1564a3aa7b9cb82
Boris Dayma <boris.dayma@gmail.com>
2022-05-05 21:27:10 +0000

    feat: add cosine schedule

---
+++ src/img_diffusion/model/diffusion.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+
+
+# Source: https://github.com/lucidrains/denoising-diffusion-pytorch
+def cosine_beta_schedule(timesteps, s=0.008):
+    """
+    cosine schedule
+    as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
+    """
+    steps = timesteps + 1
+    x = np.linspace(0, timesteps, steps)
+    alphas_cumprod = np.cos(((x / timesteps) + s) / (1 + s) * math.pi * 0.5) ** 2
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return np.clip(betas, 0, 0.999)
